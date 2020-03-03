1db1ce3ba07ee40377b4cd7b6ef922b82befb10d
d4n3tt <danettofficial@gmail.com>
2020-03-04 03:35:22 +0900

    03:35

--- src/components/Clock/index.tsx
+++ src/components/Clock/index.tsx
@@ -12,6 +12,7 @@ const Clock: React.FC<ClockProps> = ({ date }) => {
     <>
       <S.Text>
         정답입니다!
+        <br />
         {m}월 {d}일에 다음 스토리가 열립니다.
       </S.Text>
       <S.HiddenMessage></S.HiddenMessage>
