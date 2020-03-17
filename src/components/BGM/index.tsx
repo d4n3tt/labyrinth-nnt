1a2270e3d770a25e0cdfbd930910bdb2212c2fb7
d4n3tt <danettofficial@gmail.com>
2020-03-18 00:04:49 +0900

    add bgm

--- src/components/BGM/index.tsx
+++ src/components/BGM/index.tsx
@@ -5,7 +5,10 @@ const BGM: React.FC = () => {
   return (
     <>
       {window.location.pathname != `/ending` &&
-        window.location.pathname != `/ending2` && (
+        window.location.pathname != `/ending2` &&
+        window.location.pathname != `/muffan` &&
+        window.location.pathname != `/muffan2` &&
+        window.location.pathname != `/muffan3` && (
           <>
             <Sound
               url="song.mp3"
@@ -15,17 +18,29 @@ const BGM: React.FC = () => {
             />
           </>
         )}
-      {(window.location.pathname === `/ending` ||
-        window.location.pathname == `/ending2`) && (
-        <>
-          <Sound
-            url="song2.mp3"
-            playStatus="PLAYING"
-            loop={true}
-            playFromPosition={0 /* in milliseconds */}
-          />
-        </>
-      )}
+        {(window.location.pathname === `/ending` ||
+          window.location.pathname == `/ending2`) && (
+          <>
+            <Sound
+              url="song2.mp3"
+              playStatus="PLAYING"
+              loop={true}
+              playFromPosition={0 /* in milliseconds */}
+            />
+          </>
+        )}
+        {(window.location.pathname === `/muffan` ||
+          window.location.pathname == `/muffan2`) ||
+          window.location.pathname == `/muffan3`) && (
+          <>
+            <Sound
+              url="song3.mp3"
+              playStatus="PLAYING"
+              loop={true}
+              playFromPosition={0 /* in milliseconds */}
+            />
+          </>
+        )}
     </>
   );
 };
